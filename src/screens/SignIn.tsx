61671478a125de7d095c7c0af68d33230991d52f
pedroksty <pedrotrab2132@gmail.com>
2023-01-03 18:14:25 -0300

    Initial commit 🚀

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,42 @@
+import { VStack, Image, Text, Center, Heading } from 'native-base'
+
+import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from '@assets/background.png'
+import { Input } from '@components/Input'
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700" px={10} >
+      <Image 
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+
+      <Center my={24} >
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">Treine sua mente e o seu corpo</Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading" >Acesse sua conta</Heading>
+      </Center>
+
+      <Input 
+        placeholder="E-mail"
+        keyboardType='email-address'
+        autoCapitalize='none'
+      />
+
+      <Input 
+        placeholder="Senha"
+        secureTextEntry
+      />
+
+
+        
+    </VStack>
+  )
+}
